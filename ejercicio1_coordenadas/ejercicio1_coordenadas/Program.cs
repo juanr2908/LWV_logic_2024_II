6ee0c85b9bea523dfbb7d6866218d66a9d72d8d6
Juan Gonzalez <juanrafa1592@hotmail.com>
2024-09-08 18:38:09 -0500

    guaradamos avance de mi codigo

--- ejercicio1_coordenadas/ejercicio1_coordenadas/Program.cs
+++ ejercicio1_coordenadas/ejercicio1_coordenadas/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        double x1, x2, y1, y2, d;
+        Console.Write("favor ingrese el valor de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+    }
